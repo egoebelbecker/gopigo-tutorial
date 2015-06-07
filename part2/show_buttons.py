0f7a7bfbc7d793f5a200f87d8f69e2971a916167
egoebelbecker <eric@furrybytes.com>
2015-06-07 06:09:42 +0000

    Fix comments. Change stop buttons.

--- part2/show_buttons.py
+++ part2/show_buttons.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
 ###############################################################                                                                  
-# This is the first script in the tutorial at
-# (needs final link for next post)
+# Displays gamepad button keycodes
 #
 # History
 # ------------------------------------------------
@@ -13,28 +12,14 @@
 # (http://creativecommons.org/licenses/by-sa/3.0/)           
 #
 ###############################################################
+from select import select
+from evdev import InputDevice, categorize, ecodes
 from evdev import InputDevice, categorize, ecodes, KeyEvent
-from gopigo import *
-
 gamepad = InputDevice('/dev/input/event0')
 
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
         keyevent = categorize(event)
         if keyevent.keystate == KeyEvent.key_down:
-            if keyevent.keycode[0] == 'BTN_A':
-                print "Back"
-                bwd()
-            elif keyevent.keycode == 'BTN_Y':
-                print "Forward"
-                fwd()
-            elif keyevent.keycode == 'BTN_B':
-                print "Right"
-                right()
-            elif keyevent.keycode == 'BTN_X':
-                print "Left"
-                left()
-            elif keyevent.keycode == 'BTN_THUMBR' or keyevent.keycode == 'BTN_THUMBL':
-                print "Stop"
-                stop()
+            print keyevent.keycode
 
