25031185a11fa1837ebccc32458a155549161b87
Eric Goebelbecker <egoebelbecker@users.noreply.github.com>
2015-06-11 20:53:22 -0400

    Final clean up and comment

--- part1/gamepad.py
+++ part1/gamepad.py
@@ -8,6 +8,7 @@
 # ------------------------------------------------
 # Author                Date      		Comments
 # Eric Goebelbecker     Jun 5 2015 		Initial Authoring
+# Eric Goebelbecker     Jun 10 2015     Final version ?
 # 			                                                         
 # These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
 # (http://creativecommons.org/licenses/by-sa/3.0/)           
